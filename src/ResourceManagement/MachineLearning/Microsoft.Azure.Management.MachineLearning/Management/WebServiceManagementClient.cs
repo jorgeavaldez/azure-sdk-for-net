6fee5881392c2948ba3a9564b17b0940c9676509
Jorge Valdez <t-jovald@microsoft.com>
2016-07-07 16:42:31 -0700

    working on auth, finishing the management client

---
+++ src/ResourceManagement/MachineLearning/Microsoft.Azure.Management.MachineLearning/Management/WebServiceManagementClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.MachineLearning
+{
+    public class WebServiceManagementClient
+    {
+
+        WebServiceManagementClient()
+        {
+            
+        }
+
+        WebService CreateWebService(string webServiceDefintionFilePath)
+        {
+            
+        }
+    }
+}
